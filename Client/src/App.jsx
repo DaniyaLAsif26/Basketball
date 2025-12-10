f1e98f4666c76affa7b98923b10dd28fe710a7e7
Daniyal Asif <daniyalasif550@gmail.com>
2025-12-10 19:13:34 +0530

    added tourny component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,6 +3,7 @@ import SliderCont from './components/Slider/Slider-cont'
 import News from './components/News/News.jsx'
 import Line from './components/Line/line.jsx'
 import Ranking from './components/Ranking/Ranking.jsx'
+import Tournament from './components/Tournament/Tournament.jsx'
 import './App.css'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <News />
       <Line />
       <Ranking />
+      <Tournament />
     </>
   )
 }
---
+++ Client/src/components/Tournament/Tournament.jsx
@@ -0,0 +1,16 @@
+import './tournament.css';
+
+export default function Tournament() {
+    return (
+        <div className="tournament-home">
+            <div className="tournament-cont">
+                <div className="tournament-info">
+                    <h2>PARTICIPATE & HOST TOURNAMENTS</h2>
+                    <p>Players across Telangana can register on our website and stay updated about upcoming state-level tournaments and leagues. </p>
+                    <p>Organizers can also host their own basketball tournaments with ease on the platform.</p>
+                    <button className='tournament-btn'>Lets's Go</button>
+                </div>
+            </div>
+        </div>
+    )
+}
