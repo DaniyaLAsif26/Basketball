2399385173d81be27f8c3e85503dfdf5fc05e3e3
Daniyal Asif <daniyalasif550@gmail.com>
2025-12-23 17:04:35 +0530

    added home slider

--- Client/src/components/Slider/Slider-cont.jsx
+++ Client/src/components/Slider/Slider-cont.jsx
@@ -1,22 +1,48 @@
+import './slider.css';
+import { useRef } from 'react';
+
 import Slider_img from '../../assets/home-bg.jpg';
 import slide_1 from '../../assets/slide-1.jpg';
 import slide_2 from '../../assets/slide-2.jpg';
 import slide_3 from '../../assets/slide-3.jpg';
 import slide_4 from '../../assets/slide-4.jpg';
-import './slider.css'
 
 export default function SliderCont() {
+
+    const SlideRef = useRef(null)
+
+    const handleTouchStart = () => {
+        if (SlideRef.current) {
+            SlideRef.current.style.animationPlayState = 'paused';
+        }
+    };
+
+    const handleTouchEnd = () => {
+        if (SlideRef.current) {
+            SlideRef.current.style.animationPlayState = 'running';
+        }
+    };
+
     return (
         <div className="slider-cont">
             <div className="slider-img">
-            <img src={Slider_img} alt="" />
+                <img src={Slider_img} alt="Background" />
             </div>
-            <div className="slider">
-                <img src={slide_1} alt="" />
-                <img src={slide_2} alt="" />
-                <img src={slide_3} alt="" />
-                <img src={slide_4} alt="" />
+            <div className="slider"
+                ref={SlideRef}
+                onTouchStart={handleTouchStart}
+                onTouchEnd={handleTouchEnd}
+            >
+                <img src={slide_1} alt="Slide 1" />
+                <img src={slide_2} alt="Slide 2" />
+                <img src={slide_3} alt="Slide 3" />
+                <img src={slide_4} alt="Slide 4" />
+                {/* Duplicate for seamless loop */}
+                <img src={slide_1} alt="Slide 1" />
+                <img src={slide_2} alt="Slide 2" />
+                <img src={slide_3} alt="Slide 3" />
+                <img src={slide_4} alt="Slide 4" />
             </div>
         </div>
-    )
+    );
 }
