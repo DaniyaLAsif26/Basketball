2c89f710b1a9de61da77e7e310669120e55516bf
Daniyal Asif <daniyalasif550@gmail.com>
2025-12-23 09:06:32 +0530

    Changes to home tournament

--- Client/src/components/Tournament/Tournament.jsx
+++ Client/src/components/Tournament/Tournament.jsx
@@ -1,6 +1,10 @@
 import './tournament.css';
+import { useNavigate } from 'react-router-dom'
 
 export default function Tournament() {
+
+    const navigate = useNavigate()
+
     return (
         <div className="tournament-home">
             <div className="tournament-cont">
@@ -8,7 +12,7 @@ export default function Tournament() {
                     <h2>PARTICIPATE & HOST TOURNAMENTS</h2>
                     <p>Players across Telangana can register on our website and stay updated about upcoming state-level tournaments and leagues. </p>
                     <p>Organizers can also host their own basketball tournaments with ease on the platform.</p>
-                    <button className='tournament-btn'>Lets's Go</button>
+                    <button className='tournament-btn' onClick={()=> navigate('/events')}>Lets's Go</button>
                 </div>
             </div>
         </div>
