cebb42384eb62648152fced985b769a0a85be2e4
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-10 00:53:51 +0530

    Added get news route

--- Server/routes/News.js
+++ Server/routes/News.js
@@ -4,6 +4,7 @@ import News from '../models/news.js'
 import multer from 'multer'
 
 import { createNewsSchema } from '../Validators/NewsValidators.js'
+import news from '../models/news.js'
 
 const router = express.Router()
 
@@ -38,6 +39,29 @@ router.post('/admin/create', upload.single('newsImage'), async (req, res) => {
 })
 
 router.get('/all-news', async (req, res) => {
+    try {
+        const news = await News.find().sort({ createdAt: -1 })
+
+        if (!news || news.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No News Found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            news: news
+        })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json({
+            success: false,
+            message: 'Error fetching news',
+            error: err.message
+        });
+    }
 
 })
 
