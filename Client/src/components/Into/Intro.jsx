e85f9e859af70090a85990a9a75aee001952c0dc
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-16 15:32:21 +0530

    Added hero image links

--- Client/src/components/Into/Intro.jsx
+++ Client/src/components/Into/Intro.jsx
@@ -1,7 +1,11 @@
 import './intro.css'
 import into_bg from '../../assets/intro-bg.jpg'
+import {useNavigate} from 'react-router-dom'
 
 export default function Intro() {
+
+    const navigate = useNavigate();
+
     return (
         <div className="intro-cont">
             <div className="intro-head">
@@ -10,8 +14,8 @@ export default function Intro() {
                 </h1>
                 <p>"Governing & Growing Basketball Across Telangana"</p>
                 <div className="intro-btns">
-                    <button class="comic-button">Events</button>
-                    <button class=" comic-button comic-button-2">About Us</button>
+                    <button class="comic-button" onClick={() => navigate('/events')}>Events</button>
+                    <button class=" comic-button comic-button-2" onClick={() => navigate('/about')}>About Us</button>
                 </div>
             </div>
             <div className="intro-bg-img">
