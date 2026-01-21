ff94e4e566a2cf0b268bbb9fa067f76e3997984d
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-21 16:48:34 +0530

    Added User Schema Validators

---
+++ Server/Validators/UserValidators.js
@@ -0,0 +1,32 @@
+import { z } from 'zod'
+
+export const createUserSchema = z.object({
+    firstName: z.string().min(3),
+    lastName: z.string().min(1),
+    profilePicture: z.string().optional(),
+    phoneNumber: z.string().regex(/^[6-9]\d{9}$/).min(10).max(10),
+    homeTown: z.string().min(1),
+    dateOfBirth: z.coerce.date(),
+    height: z.string().min(1),
+    weight: z.string().min(1),
+    wingspan: z.string().min(1),
+    playerPosition: z.enum(['Point Guard', 'Shooting Guard', 'Small Forward', 'Power Forward', 'Center']),
+})
+    .refine(
+        data => {
+            const dob = new Date(data.dateOfBirth);
+            const today = new Date();
+
+            const minAgeDate = new Date(
+                today.getFullYear() - 10,
+                today.getMonth(),
+                today.getDate()
+            );
+
+            return dob <= minAgeDate;
+        },
+        {
+            message: "User must be at least 10 years old",
+            path: ["dateOfBirth"],
+        }
+    )
