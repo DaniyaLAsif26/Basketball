d98c4582ec12b2e65d047545c37fd5b8f6067588
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-31 02:50:22 +0530

    added classname to intro btns

--- Client/src/components/Into/Intro.jsx
+++ Client/src/components/Into/Intro.jsx
@@ -7,15 +7,15 @@ export default function Intro() {
     const navigate = useNavigate();
 
     return (
-        <div className="intro-cont-">
+        <div className="intro-cont">
             <div className="intro-head">
                 <h1>
                     TELANGANA&nbsp; BASKETBALL&nbsp; ASSOCIATION
                 </h1>
                 <p>"Governing & Growing Basketball Across Telangana"</p>
                 <div className="intro-btns">
-                    <button class="comic-button" onClick={() => navigate('/events')}>Events</button>
-                    <button class=" comic-button comic-button-2" onClick={() => navigate('/about')}>About Us</button>
+                    <button className="comic-button" onClick={() => navigate('/events')}>Events</button>
+                    <button className="comic-button comic-button-2" onClick={() => navigate('/about')}>About Us</button>
                 </div>
             </div>
             <div className="intro-bg-img">
