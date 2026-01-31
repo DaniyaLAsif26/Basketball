a762a3fa48f9d6adc42ae37bc401b8c732d9d503
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-31 12:46:08 +0530

    Intro cont fix 5

--- Client/src/components/Into/Intro.jsx
+++ Client/src/components/Into/Intro.jsx
@@ -1,26 +1,27 @@
-import './intro.css'
 import into_bg from '../../assets/intro-bg.jpg'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
+import './intro.css'
 
 export default function Intro() {
     const navigate = useNavigate();
 
     return (
-        <div className="intro-cont">
-            <div className="intro-bg-img">
-                <img src={into_bg} alt="" />
-            </div>
-            <div className="intro-bg-color"></div>
-            <div className="intro-head">
-                <h1>
-                    TELANGANA&nbsp; BASKETBALL&nbsp; ASSOCIATION
-                </h1>
-                <p>"Governing & Growing Basketball Across Telangana"</p>
-                <div className="intro-btns">
-                    <button className="comic-button" onClick={() => navigate('/events')}>Events</button>
-                    <button className="comic-button comic-button-2" onClick={() => navigate('/about')}>About Us</button>
+            <div className="intro-cont">
+                <div className="intro-bg-img">
+                    <img src={into_bg} alt="" />
+                </div>
+                <div className="intro-bg-color"></div>
+                <div className="intro-head">
+                    <h1>
+                        TELANGANA&nbsp; BASKETBALL&nbsp; ASSOCIATION
+                    </h1>
+                    <p>"Governing & Growing Basketball Across Telangana"</p>
+                    <div className="intro-btns">
+                        <button className="comic-button" onClick={() => navigate('/events')}>Events</button>
+                        <button className="comic-button comic-button-2" onClick={() => navigate('/about')}>About Us</button>
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
