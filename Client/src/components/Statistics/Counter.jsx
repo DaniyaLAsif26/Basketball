e832d87beef54cd1e7ebdd13bd5f549601611666
Daniyal Asif <daniyalasif550@gmail.com>
2026-01-31 14:29:47 +0530

    added statistics heading

--- Client/src/components/Statistics/Counter.jsx
+++ Client/src/components/Statistics/Counter.jsx
@@ -5,13 +5,28 @@ import { TbPlayBasketball } from "react-icons/tb";
 import { GiWhistle } from "react-icons/gi";
 import { FaTrophy } from "react-icons/fa6";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { useEffect ,useState } from 'react';
 
 export default function Counter() {
+
+    const [sliderWidth, setSliderWidth] = useState(window.innerWidth < 560)
+
+    useEffect(() => {
+        const handleResize = () => {
+            setSliderWidth(window.innerWidth < 999)
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [])
     return (
         <div className="counter">
             <div className="counter-cont">
                 <div className="counter-cont-head">
-                    <h2>Powering the Growth of Basketball in Telangana</h2>
+                    <h2> {sliderWidth ? 'Powering Telangana Basketball' : 'Powering the Growth of Basketball in Telangana'}</h2>
                 </div>
                 <div className="counter-elements-cont">
                     <CounterElement icon={<TbPlayBasketball />} label={'Players'} target={600} />
