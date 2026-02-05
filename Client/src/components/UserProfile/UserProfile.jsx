ffe53d87be866f75447718cce382e582b01f884b
Daniyal Asif <daniyalasif550@gmail.com>
2026-02-05 09:51:01 +0530

    added loading state to user profile better

--- Client/src/components/UserProfile/UserProfile.jsx
+++ Client/src/components/UserProfile/UserProfile.jsx
@@ -6,7 +6,16 @@ export default function UserProfile() {
 
     const { userData, isUserLoading } = useLogin()
 
-    if (isUserLoading) return <div>Loading...</div>;
+    if (isUserLoading)
+        return <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            fontSize: '1.5rem'
+        }}>
+            Loading...
+        </div>;
     if (!userData) return <div>No user data</div>
 
     return (
