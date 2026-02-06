392d7b0613f0dfcf56f41f8aee39fea37ed01196
Daniyal Asif <daniyalasif550@gmail.com>
2026-02-06 22:15:45 +0530

    Added loading state to all pages useLoading

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,14 +9,32 @@ import AdminNav from './components/Admin/AdminNav.jsx'
 
 import { useLocation } from 'react-router-dom'
 
+import { useLogin } from './context/LoginContext.jsx'
+import logo from '../src/assets/red.png'
+
 function App() {
   const location = useLocation()
 
-  const route = ['/login', '/admin' , '/admin/add-news' , '/admin/add-event']
-  const adminRoutes = ['/admin' , '/admin/add-news' , '/admin/add-event']
+  const route = ['/login', '/admin', '/admin/add-news', '/admin/add-event']
+  const adminRoutes = ['/admin', '/admin/add-news', '/admin/add-event']
 
   const checkRoute = route.includes(location.pathname)
   const checkAdminRoute = adminRoutes.includes(location.pathname)
+
+  const {isUserLoading } = useLogin()
+
+  if (isUserLoading) {
+    return (
+      <div className='loading-all' style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}>
+        <img src={logo} alt="" />
+      </div>
+    )
+  }
 
   return (
     <>
